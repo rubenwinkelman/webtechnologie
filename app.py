efc3c17fa5bf3a0d67dd10be905dcdcd70963bbe
WESSELS-LAPTOP\wesse <wesseljanvdveen@gmail.com>
2026-02-13 10:50:54 +0100

    Registratie pagina werkt nu. Inlog pagina misschien ook

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Een 'Model' is eigenlijk een tabel in je database
-class Gebruiker(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    gebruikersnaam = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f'<Gebruiker {self.gebruikersnaam}>'
-
+        return f'<User {self.email}>'
 
 @app.route('/')
 def home():
     return render_template('homepagina.html')
 
-@app.route('/register')
-def register():
-    return render_template('register_page.html')
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.password == password:
+            return redirect(url_for('home'))
+            
     return render_template('login_page.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        nieuwe_gebruiker = User(email=email, password=password)
+        db.session.add(nieuwe_gebruiker)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
+        
+    return render_template('register_page.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
